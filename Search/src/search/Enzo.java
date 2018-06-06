14486f9dec05092dd90f16ce58ea031a33a7f134
enzomzaocv <38295665+enzomzaocv@users.noreply.github.com>
2018-06-06 15:45:04 -0300

    agregar metodos
    metodo para contar letras, descompoonerpalabras. etc

--- Search/src/search/Enzo.java
+++ Search/src/search/Enzo.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package search;
-
+import java.io.File;
 /**
  *
- * @author user
+ * @author Enzo
  */
 public class Enzo {
+	
+	public void lector(String ubicacion) {
+		File archivo=null;
+		FileReader fileReader=null;
+		BufferedReader buffReader=null;
+		String linea;
+		try {
+			archivo= new File(ubicacion);
+			fileReader= new FileReader(archivo);
+			buffReader= new BufferedReader(fileReader);
+			
+			while((linea=buffReader.readLine())!=null) {
+				buffReader.readLine();
+			}
+			
+			buffReader.close();
+		}catch(Exception e) {}
+	}
+	
+	public void letraMasRepetida() {
+
+	}
+	
     
 }

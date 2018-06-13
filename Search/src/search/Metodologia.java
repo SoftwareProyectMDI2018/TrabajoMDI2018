07f88450e76143371bd630031a3812205b57fbb9
luxas13 <palanalucas39@gmail.com>
2018-06-13 14:29:26 -0300

    commit

---
+++ Search/src/search/Metodologia.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodologia;
+
+import java.io.Console;
+
+/**
+ *
+ * @author Luxas
+ */
+public class Metodologia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      String [] palabras={"hola","adios","saludos","etc" };
+      for (int x=0; x<palabras.length; x++){
+
+String user= String.valueOf(palabras[x]);
+          System.out.println(palabras[x]);
+}
+      char[] aCaracteres;
+      char[] aCaracteres = palabras[x].toCharArray;
+    }
+    
+}

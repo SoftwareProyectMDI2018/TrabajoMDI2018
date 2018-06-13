8999fa1074cff963c7cf0ead7b8bdf0f0389337a
Juan <38295553+00juan11@users.noreply.github.com>
2018-06-13 00:24:10 -0300

    Clase actualizada

--- Search/src/search/OrdenAlfabetico.java
+++ Search/src/search/OrdenAlfabetico.java
@@ -1,56 +1,52 @@
-/**
- * Autor: Juan
- * Fecha: 27-5-2018
- * Última modificación: 3-6-2018
- * Propósito: La clase debe poseer un método que pueda
- tomar un arreglo de Strings,ordenarlo alfabeticamente
- (en otro arreglo) y posteriormente imprimirlo.
- */
 
-public class Alphabetical {
+/*
+*Autor: juan
+*Fecha: 27-5-2018
+*Ultima modificacion: 12-6-2018
+*Propósito: Ordenar una lista de elementos(objetos) Strings
+*/
+public class OrdenAlfabetico {
     //Variables instanciadas
-    private String[] array1;
-    private String[] array2;
+    private String[] arreglo1;
+    private String[] arreglo2;
     //Constructor
-    public Alphabetical(String[] array1, String[] array2) {
+    public OrdenAlfabetico(String[] arreglo1, String[] arreglo2) {
       //El objeto se inicializará sólo si los arreglos son del mismo tamaño
-      if(array1.length == array2.length)
-          this.array1 = array1;
-          this.array2 = array2;
+      if(arreglo1.length == arreglo2.length)
+          this.arreglo1 = arreglo1;
+          this.arreglo2 = arreglo2;
     }
     //Métodos get && set
-    public String[] getArray1() {
-        return array1;
+    public String[] getArreglo1() {
+        return arreglo1;
     }
 
-    public void setArray1(String[] array1) {
-        this.array1 = array1;
+    public void setArreglo1(String[] arreglo1) {
+        this.arreglo1 = arreglo1;
     }
 
-    public String[] getArray2() {
-        return array2;
+    public String[] getArreglo2() {
+        return arreglo2;
     }
 
-    public void setArray2(String[] array2) {
-        this.array2 = array2;
+    public void setArreglo2(String[] arreglo2) {
+        this.arreglo2 = arreglo2;
     }
 
     //método que toma los elementos de un arreglo y los ordena en un arreglo nuevo
     //Teniendo en cuenta el estandar de ascii
-    public void orderedList(String[] array1, String[] array2){
-
-      if(array1.length == array2.length)
+    public void listaOrdenada(String[] arreglo1, String[] arreglo2){
+      if(arreglo1.length == arreglo2.length)
       {
-
         int k = 0;
-        int conditionControl = 0; //variable que controla la condición
+        int condicionDeControl = 0; //variable que controla la condición
         for(int i = 0; i <= 127; i++)
         {
-            for(int j = 0; j < array1.length; j++)
+            for(int j = 0; j < arreglo1.length; j++)
             {
-                if(((int)array1[j].toUpperCase().charAt(k)) == i){ //Condición clave
-                    array2[k + conditionControl] =  array1[j];
-                    conditionControl++; //Incremento
+                if(((int)arreglo1[j].toUpperCase().charAt(k)) == i){ //Condición clave
+                    arreglo2[k + condicionDeControl] =  arreglo1[j];
+                    condicionDeControl++; //Incremento
                 }//fin condición si
             }//fin segundo for
         }//fin primer for
@@ -58,15 +54,15 @@ public class Alphabetical {
       else//sino
       {
         System.out.println("El tamaño de ambos arreglos no coincide");//Se imprime un mensaje de error al usuario
+        System.out.println("No puede ejecutar este método."); //Mensaje a Usuario
       }
     }//fin método orderedList
-
     //Método displayList que muestra la lista
-    public void displaysList(String[]array)
+    public void muestraLista(String[]arreglo)
     {
-        for(int i = 0; i < array.length; i++)
+        for(int i = 0; i < arreglo.length; i++)
         {
-            System.out.println(array[i]);
+            System.out.println(arreglo[i]);
         }
     }//fin del método displaysList
-}//fin de la clase Alphabetical
+}//fin de la clase OrdenAlfabetico
